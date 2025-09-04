5e8d898aed2f817d1bb823e5f126b1c27b6b21b7
mthegn <52200125@student.tdtu.edu.vn>
2025-09-05 00:19:13 +0700

    Update fee-service and docker-compose

---
+++ backend/fee-service/src/config/db.js
@@ -0,0 +1,32 @@
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+    logging: false, // tắt log query cho gọn
+  }
+);
+
+async function connectWithRetry(retries = 10, delay = 5000) {
+  while (retries > 0) {
+    try {
+      await sequelize.authenticate();
+      console.log("✅ Database connected successfully.");
+      break;
+    } catch (err) {
+      console.error("❌ DB connection failed, retrying in 5s...", err.message);
+      retries--;
+      if (retries === 0) process.exit(1);
+      await new Promise((res) => setTimeout(res, delay));
+    }
+  }
+}
+
+connectWithRetry();
+
+export default sequelize;
---
+++ backend/fee-service/src/models/fee.model.js
@@ -0,0 +1,15 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db.js";
+
+const Fee = sequelize.define("Fee", {
+  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+  description: { type: DataTypes.STRING, allowNull: true },
+  amount: { type: DataTypes.FLOAT, allowNull: false },
+  semester: { type: DataTypes.INTEGER, allowNull: false },
+  year: { type: DataTypes.INTEGER, allowNull: false }
+}, {
+  tableName: "fees",
+  timestamps: true
+});
+
+export default Fee;
