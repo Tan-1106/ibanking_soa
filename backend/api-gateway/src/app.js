2d249475d67f5eff49cc7c24b7ea47d418dc3a5c
XungChan <nmdtruong18032004@gmail.com>
2025-09-12 13:22:13 +0700

    fix: start api-gateway
    -add RUN apk add --no-cache curl
    to Dockerfile

--- backend/api-gateway/src/app.js
+++ backend/api-gateway/src/app.js
@@ -5,13 +5,23 @@ import cors from "cors";
 import authRoutes from "./routes/auth.routes.js";
 import studentRoutes from "./routes/student.routes.js";
 import paymentRoutes from "./routes/payment.routes.js";
+import proxy from "express-http-proxy";
 
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-
+const user = proxy("http://user-service:4001");
+const student = proxy("http://student-service:4002");
+const payment = proxy("http://payment-service:4003");
+const otp = proxy("http://otp-service:4004");
+const notification = proxy("http://notification-service:4005");
+app.use("/user", user);
+app.use("/student", student);
+app.use("/payment", payment);
+app.use("/otp", otp);
+app.use("/notification", notification);
 // Routes
 app.use("/auth", authRoutes);
 app.use("/student", studentRoutes);
